31db83a3cd5ab36e7b7491ffc9268a1f1d188e05
ymink716 <ymink716@gmail.com>
2019-11-02 14:08:56 +0900

    feat: topping 스키마 수정

--- schemas/subclass.js
+++ schemas/subclass.js
@@ -2,13 +2,21 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const Subclass = new Schema({
+
+const SubclassSchema = new Schema({
     name: {
         type: String,
         required: true,
         unique: true
     },
+    category: {
+        type: String,
+        required: true,
+    },
     image: {
-        
+        type: String,
+        required: true,
     }
 });
+
+module.exports = mongoose.model('Subclass', SubclassSchema);
--- schemas/topping.js
+++ schemas/topping.js
@@ -1,20 +1,18 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
+const { Types: { ObjectId } } = Schema;
 
 const toppingSchema = new Schema({
     name: {
         type: String,
         required: true,
     },
-    category: {
-        type: String,
-        required: true,
-    },
     subclass: {
-        type: Object, // { name, image url }
+        type: ObjectId,
         required: true,
-    },
+        ref: 'Subclass',
+    }
 });
 
 module.exports = mongoose.model('Topping', toppingSchema);
