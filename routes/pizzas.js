8dff9f88c62dab8a56e30ffeb806753ece84a957
ymink716 <ymink716@gmail.com>
2019-11-16 14:07:15 +0900

    feat: pizzaService 코드 수정

--- routes/pizzas.js
+++ routes/pizzas.js
@@ -1,53 +1,12 @@
 import { Router } from 'express';
-import Pizza from '../schemas/pizza';
 import pizzaService from '../services/pizzaService';
-import Subclass from '../schemas/subclass';
 
 const router = Router();
 
-router.post('/recomandations', async (req, res, next) => {
-    try {
-        const recomandations = [];
-        let items = req.body.items;
-        if(!items){
-            return res.json({
-                result: "no item"
-            })
-        }
-        console.log(items);
-        items = JSON.parse(items);    
-        const pizzas = await Pizza.find({},{brand:1, name:1, m_price:1, m_cal:1, subclasses:1, image: 1});
-        pizzas.forEach(pizza => {
-            if (items.every(x => pizza.subclasses.indexOf(x) !== -1)) {
-                console.log(pizza.name)
-                pizza.subclasses = undefined
-                recomandations.push(pizza);
-            }
-        });
-        let pizzaNum = recomandations.length;
-        res.json({
-            num: pizzaNum,
-            pizzas:recomandations
-        });
-    } catch (error) {
-        console.error(error);
-        next(error);
-    } 
-});
-
-router.get('/details/:id', async (req, res, next) => { // 피자 상세 정보
-    try {
-        const pizza = await Pizza.find({ _id: req.params.id });
-        res.json(pizza);
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
-
+router.post("/recomandations", pizzaService.recommandPizzas);
+router.get("/details/:id", pizzaService.getDetails);
 router.get("/toppings", pizzaService.readToppings);
 router.get("/result", pizzaService.readTopping);
 router.post("/find", pizzaService.findPizza);
-
 
 module.exports = router;
--- services/pizzaService.js
+++ services/pizzaService.js
@@ -2,6 +2,44 @@ import Pizza from '../schemas/pizza';
 import SubClass from '../schemas/subclass';
 import Topping from '../schemas/topping';
 
+const recommandPizzas = async (req, res, next) => { 
+    try {
+        const recomandations = [];
+        let items = req.body.items;
+        if(!items){
+            return res.json({
+                result: "no item"
+            })
+        }
+        items = JSON.parse(items);    
+        const pizzas = await Pizza.find({},{brand:1, name:1, m_price:1, m_cal:1, subclasses:1, image: 1});
+        pizzas.forEach(pizza => {
+            if (items.every(x => pizza.subclasses.indexOf(x) !== -1)) {
+                pizza.subclasses = undefined;
+                recomandations.push(pizza);
+            }
+        });
+        let pizzaNum = recomandations.length;
+        res.json({
+            num: pizzaNum,
+            pizzas:recomandations
+        });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }  
+}
+
+const getDetails = async (req, res, next) => {
+    try {
+        const pizza = await Pizza.find({ _id: req.params.id });
+        res.json(pizza);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+}
+
 const readToppings = async (req, res) =>{
     const meat = []; 
     const seafood = [];
