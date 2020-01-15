28ff860e6b1c9b2838224edb62a1696b5c1f974c
ymink716 <ymink716@gmail.com>
2020-01-15 13:56:33 +0900

    etc: port 변경

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`server is running on ${port}`)
